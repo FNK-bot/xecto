93d69cfc92184a56f5fd63426b954136535a6652
Farseen Nk <farseennk2@gmail.com>
2025-11-20 11:52:24 +0000

    no i need the <> icon but it need to link in the header like link rel ic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path d='M20 20 L80 80 M80 20 L20 80' stroke='black' stroke-width='12' stroke-linecap='round' stroke-linejoin='round'/></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
