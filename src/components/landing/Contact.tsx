8e45787c8f1576a1422cb542c67222c15c6b1526
Farseen Nk <farseennk2@gmail.com>
2025-11-20 11:21:14 +0000

    fix eroors

--- src/components/landing/Contact.tsx
+++ src/components/landing/Contact.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useFormState } from "react-dom";
-import { useFormStatus } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import {
   Card,
